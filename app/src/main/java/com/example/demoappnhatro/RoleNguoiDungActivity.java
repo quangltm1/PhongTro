698371dc61f9015dbf770c58ce83f81b6a21312f
Nhan <ngothanhnhaneric@gmail.com>
2023-07-29 00:40:14 +0700

    push nhieu thu

---
+++ app/src/main/java/com/example/demoappnhatro/RoleNguoiDungActivity.java
@@ -0,0 +1,14 @@
+package com.example.demoappnhatro;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RoleNguoiDungActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_role_nguoi_dung);
+    }
+}
---
+++ app/src/main/java/com/example/demoappnhatro/ThongTinHopDongActivity.java
@@ -0,0 +1,14 @@
+package com.example.demoappnhatro;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ThongTinHopDongActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_thong_tin_hop_dong);
+    }
+}
